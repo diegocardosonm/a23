6b7a81f01dd92e4c8f001a8bc005ddbbce2f8318
diegocardosonm <futurotechia@gmail.com>
2025-06-11 00:33:28 +0000

    Add JavaScript to validate email format

---
+++ a23/js/validate.js
@@ -0,0 +1,8 @@
+document.getElementById("form").addEventListener("submit", function(event) {
+    var email = document.getElementById("email").value;
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert("Por favor, insira um email válido.");
+        event.preventDefault();
+    }
+});
